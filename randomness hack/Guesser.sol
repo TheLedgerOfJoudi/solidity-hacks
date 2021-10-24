78e40e291f28e3628e30de66b22dd2320b4d83d1
hadi <hadi.mohammad.saleh1@gmail.com>
2021-10-24 10:08:58 +0300

    quick fixes and formatting

--- randomness hack/Guesser.sol
+++ randomness hack/Guesser.sol
@@ -2,12 +2,11 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Guesser {
-    constructor () payable {}
-    
-    function guess(uint _guess) public payable{
-        require (msg.value == 1 ether);
-        uint rand =
-        uint (
+    constructor() payable {}
+
+    function guess(uint256 _guess) public payable {
+        require(msg.value == 1 ether);
+        uint256 rand = uint256(
             keccak256(
                 abi.encodePacked(
                     blockhash(block.number - 1),
@@ -15,19 +14,18 @@ contract Guesser {
                     block.coinbase,
                     block.difficulty,
                     block.coinbase
-                    )
                 )
-            );
-            if ((rand + _guess) % 2 == 0){
-               payable(msg.sender).transfer(2 ether);
-            }
+            )
+        );
+        if ((rand + _guess) % 2 == 0) {
+            payable(msg.sender).transfer(2 ether);
+        }
     }
-    
-    receive() external payable{}
-    function getBalance()public view returns (uint) {
+
+    receive() external payable {}
+
+    function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
-    function hat(address payable addr) public payable{
-        selfdestruct(addr);
-    }
+
 }
