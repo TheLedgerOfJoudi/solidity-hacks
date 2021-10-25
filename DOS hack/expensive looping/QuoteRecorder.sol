294e9ff329b71ae1d451c719af00f80641af4731
hadi <hadi.mohammad.saleh1@gmail.com>
2021-10-25 11:05:27 +0300

    fixes

---
+++ DOS hack/expensive looping/QuoteRecorder.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract QuoteRecorder {
+    address payable public currentQuoteLover;
+    address public owner;
+    uint256 public balance;
+    string public currentQuote;
+    address[] public recorders;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function record(string memory quote) external payable {
+        require(msg.value > balance);
+        currentQuoteLover.transfer(balance);
+        balance = msg.value;
+        currentQuoteLover = payable(msg.sender);
+        currentQuote = quote;
+        recorders.push(msg.sender);
+    }
+
+    function reward() public view {
+        require(msg.sender == owner);
+        for (uint256 i = 0; i < recorders.length; i++) {
+            distributeToken(recorders[i], 10);
+        }
+    }
+}
